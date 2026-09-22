f6bf8d83fdc469c194d877917bb582117d179b99
Akash <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-09-22 16:36:45 +0530

    update schema.yml and models

--- banking_dbt/models/marts/fact/fct_transactions.sql
+++ banking_dbt/models/marts/fact/fct_transactions.sql
@@ -1,4 +1,4 @@
-{{config( materialized='incremental', unique_key='transaction_id')}}
+{{config(materialized='incremental', unique_key='transaction_id')}}
 
 WITH enriched_transactions AS (
     SELECT
